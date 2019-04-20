0343943b5913623c4c8e33402f53319dcc21d991
lMINERl <seect19944@gmail.com>
2019-04-20 13:22:02 +0200

    no message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
